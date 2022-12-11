73bcc44a38f0627fb90bd7fec5a9dbaa2a5c2cce
sohrab09 <sohrab@nassagroup.org>
2022-12-11 15:53:13 +0600

    MessageInput Handle

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -1,14 +1,122 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform
+} from "react-native";
+import React, { useState } from "react";
+import {
+  SimpleLineIcons,
+  Feather,
+  MaterialCommunityIcons,
+  AntDesign
+} from "@expo/vector-icons";
 
 const MessageInput = () => {
+  const [message, setMessage] = useState("");
+
+  const onMicrophonePress = () => {
+    console.warn("Microphone");
+  };
+
+  const handleSendMessage = () => {
+    console.warn(`Sending: ${message}`);
+    setMessage("");
+  };
+
+  const onPlusClicked = () => {
+    console.warn("On Plus Clicked");
+  };
+
+  const onPress = () => {
+    if (message) {
+      handleSendMessage();
+    } else {
+      onPlusClicked();
+    }
+  };
+
   return (
-    <View>
-      <Text>MessageInput</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.root}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={100}
+    >
+      <View style={styles.inputContainer}>
+        <SimpleLineIcons
+          name="emotsmile"
+          size={24}
+          color="#595959"
+          style={styles.icon}
+        />
+        <TextInput
+          placeholder="Send message ..."
+          style={styles.textInput}
+          value={message}
+          onChangeText={setMessage}
+        />
+        <Feather name="camera" size={24} color="#595959" style={styles.icon} />
+        <MaterialCommunityIcons
+          name="microphone"
+          size={24}
+          color="#595959"
+          style={styles.icon}
+          onPress={onMicrophonePress}
+        />
+      </View>
+      <Pressable style={styles.buttonContainer} onPress={onPress}>
+        {!message ? (
+          <AntDesign name="plus" size={24} color="white" />
+        ) : (
+          <MaterialCommunityIcons
+            name="send"
+            size={24}
+            color="white"
+            style={styles.icon}
+          />
+        )}
+      </Pressable>
+    </KeyboardAvoidingView>
   );
 };
 
 export default MessageInput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: "row",
+    padding: 10
+  },
+  inputContainer: {
+    backgroundColor: "#f2f2f2",
+    borderWidth: 1,
+    borderColor: "#dedede",
+    flex: 1,
+    marginRight: 10,
+    borderRadius: 25,
+    alignItems: "center",
+    flexDirection: "row",
+    padding: 5
+  },
+  textInput: {
+    flex: 1,
+    marginHorizontal: 5
+  },
+  icon: {
+    marginHorizontal: 5
+  },
+  buttonContainer: {
+    backgroundColor: "#3777f0",
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 25
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 35
+  }
+});
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, FlatList, View } from "react-native";
 import React from "react";
 import Message from "../components/Message/Message";
 import chatRoomData from "../assets/dummy-data/Chats";
+import MessageInput from "../components/MessageInput/MessageInput";
 
 const ChatRoomScreen = () => {
   return (
@@ -11,6 +12,7 @@ const ChatRoomScreen = () => {
         renderItem={({ item }) => <Message message={item} />}
         keyExtractor={(item) => item.id}
       />
+      <MessageInput />
     </View>
   );
 };
